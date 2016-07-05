6318be33869f79b60fd5e0d964a78d41e5b34462
Marko Rautajoki <marko.rautajoki@gofore.com>
2016-07-05 13:11:37 +0300

    Added test for import status cleanup and some refactoring of tests

--- src/test/java/fi/helsinki/moodi/web/AbstractSuccessfullCreateCourseTest.java
+++ src/test/java/fi/helsinki/moodi/web/AbstractSuccessfullCreateCourseTest.java
@@ -31,7 +31,7 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 public abstract class AbstractSuccessfullCreateCourseTest extends AbstractCourseControllerTest {
 
     protected static final long COURSE_REALISATION_ID = 102374742L;
-    private final static long MOODLE_COURSE_ID = 988888;
+    protected final static long MOODLE_COURSE_ID = 988888L;
     private static final long MOODLE_USER_ID_1 = 1L;
     private static final long MOODLE_USER_ID_2 = 2L;
     private static final long MOODLE_USER_ID_3 = 3L;
