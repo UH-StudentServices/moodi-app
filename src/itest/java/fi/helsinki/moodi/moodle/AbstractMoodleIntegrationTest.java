3ccd7d011b520b85ae1497a1eac2c442a24227a5
Marko Rautajoki <marko.rautajoki@gofore.com>
2016-12-30 11:08:18 +0200

    Moodle integration Synchronization tests

--- src/itest/java/fi/helsinki/moodi/moodle/AbstractMoodleIntegrationTest.java
+++ src/itest/java/fi/helsinki/moodi/moodle/AbstractMoodleIntegrationTest.java
@@ -1,24 +1,71 @@
 package fi.helsinki.moodi.moodle;
 
+import fi.helsinki.moodi.integration.moodle.MoodleClient;
+import fi.helsinki.moodi.integration.moodle.MoodleUserEnrollments;
+import fi.helsinki.moodi.service.importing.ImportCourseRequest;
+import fi.helsinki.moodi.service.importing.ImportingService;
+import fi.helsinki.moodi.service.util.MapperService;
 import fi.helsinki.moodi.test.AbstractMoodiIntegrationTest;
 import org.junit.After;
 import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.client.MockRestServiceServer;
+
+import java.util.List;
+import java.util.Random;
+
+import static fi.helsinki.moodi.integration.esb.EsbService.DOMAIN_SUFFIX;
+import static java.lang.Math.abs;
 
 public abstract class AbstractMoodleIntegrationTest extends AbstractMoodiIntegrationTest {
 
+    @Autowired
+    private ImportingService importingService;
+
+    @Autowired
+    protected MoodleClient moodleClient;
+
+    @Autowired
+    protected MapperService mapperService;
+
+    protected static final String STUDENT_NUMBER = "014010293";
+    protected static final String STUDENT_USERNAME = "bar_simp";
+    protected static final String TEACHER_ID = "011631484";
+    protected static final String TEACHER_USERNAME = "mag_simp";
+    protected static final String STUDENT_NUMBER_2 = "011911609";
+    protected static final String STUDENT_USERNAME_2 = "mag_simp";
+
+    protected static final String INTEGRATION_TEST_OODI_FIXTURES_PREFIX = "src/itest/resources/fixtures/oodi/";
+
+
     @Before
     @Override
-    public void setUpMockServers() {
+    public final void setUpMockServers() {
         oodiMockServer = MockRestServiceServer.createServer(oodiRestTemplate);
         esbMockServer = MockRestServiceServer.createServer(esbRestTemplate);
     }
 
     @After
     @Override
-    public void verify() {
+    public final void verify() {
         oodiMockServer.verify();
         esbMockServer.verify();
+    }
+
+    protected long getOodiCourseId() {
+        return abs(new Random().nextInt());
+    }
+
+    protected long importCourse(long courseId) {
+        return importingService.importCourse(new ImportCourseRequest(courseId)).data
+            .map(c -> c.moodleCourseId).orElseThrow(() -> new RuntimeException("Course import failed"));
+    }
+
+    protected MoodleUserEnrollments findEnrollmentsByUsername(List<MoodleUserEnrollments> moodleUserEnrollmentsList, String username) {
+        return moodleUserEnrollmentsList.stream()
+            .filter(e -> (username + DOMAIN_SUFFIX).equals(e.username))
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Enrollment not found for " + username));
     }
 
 }
--- src/itest/java/fi/helsinki/moodi/moodle/MoodleIntegrationSynchronizeCourseTest.java
+++ src/itest/java/fi/helsinki/moodi/moodle/MoodleIntegrationSynchronizeCourseTest.java
@@ -19,8 +19,12 @@ package fi.helsinki.moodi.moodle;
 
 import com.google.common.collect.ImmutableMap;
 import fi.helsinki.moodi.integration.moodle.MoodleUserEnrollments;
+import fi.helsinki.moodi.service.course.CourseRepository;
+import fi.helsinki.moodi.service.synchronize.SynchronizationService;
+import fi.helsinki.moodi.service.synchronize.SynchronizationType;
 import fi.helsinki.moodi.test.fixtures.Fixtures;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 import java.util.List;
@@ -31,15 +35,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-public class MoodleIntegrationImportCourseTest extends AbstractMoodleIntegrationTest {
+public class MoodleIntegrationSynchronizeCourseTest extends AbstractMoodleIntegrationTest {
+
+    @Autowired
+    private SynchronizationService synchronizationService;
+
+    @Autowired
+    private CourseRepository courseRepository;
 
     @Test
-    public void testMoodleIntegrationWhenImportingCourse() {
+    public void testMoodleIntegrationWhenSyncingCourse() {
+        courseRepository.deleteAll(); // Delete migrated courses for easier testing
+
         long oodiCourseId = getOodiCourseId();
 
         expectCourseImport(oodiCourseId);
 
+        expectGetCourseUsers(oodiCourseId);
+
         long moodleCourseId = importCourse(oodiCourseId);
+
+        synchronizationService.synchronize(SynchronizationType.FULL);
 
         List<MoodleUserEnrollments> moodleUserEnrollmentsList = moodleClient.getEnrolledUsers(moodleCourseId);
 
@@ -49,13 +65,13 @@ public class MoodleIntegrationImportCourseTest extends AbstractMoodleIntegration
         MoodleUserEnrollments teacherEnrollment = findEnrollmentsByUsername(moodleUserEnrollmentsList, TEACHER_USERNAME);
 
         assertEquals(2, studentEnrollment.roles.size());
-        assertTrue(studentEnrollment.hasRole(mapperService.getMoodiRoleId()));
         assertTrue(studentEnrollment.hasRole(mapperService.getStudentRoleId()));
+        assertTrue(studentEnrollment.hasRole(mapperService.getMoodiRoleId()));
 
-        assertEquals(2, teacherEnrollment.roles.size());
+        assertEquals(3, teacherEnrollment.roles.size());
         assertTrue(teacherEnrollment.hasRole(mapperService.getMoodiRoleId()));
+        assertTrue(teacherEnrollment.hasRole(mapperService.getStudentRoleId()));
         assertTrue(teacherEnrollment.hasRole(mapperService.getTeacherRoleId()));
-
     }
 
     private void expectCourseImport(long courseId) {
@@ -64,18 +80,38 @@ public class MoodleIntegrationImportCourseTest extends AbstractMoodleIntegration
             courseId,
             withSuccess(Fixtures.asString(
                 INTEGRATION_TEST_OODI_FIXTURES_PREFIX,
-                "course-realisation-import.json",
+                "course-realisation-sync-1.json",
+                new ImmutableMap.Builder()
+                    .put("courseId", courseId)
+                    .put("teacherId", TEACHER_ID)
+                    .put("endDate", getFutureDateString())
+                    .build()),
+                MediaType.APPLICATION_JSON));
+
+        expectFindEmployeeRequestToEsb(TEACHER_ID_PREFIX + TEACHER_ID, TEACHER_USERNAME);
+    }
+
+    private void expectGetCourseUsers(long courseId) {
+        expectGetCourseUsersRequestToOodi(
+            courseId,
+            withSuccess(Fixtures.asString(
+                INTEGRATION_TEST_OODI_FIXTURES_PREFIX,
+                "course-realisation-sync-2.json",
                 new ImmutableMap.Builder()
                     .put("courseId", courseId)
                     .put("studentNumber", STUDENT_NUMBER)
                     .put("studentApproved", true)
+                    .put("studentNumber2", STUDENT_NUMBER_2)
+                    .put("studentApproved2", true)
                     .put("teacherId", TEACHER_ID)
                     .put("endDate", getFutureDateString())
                     .build()),
                 MediaType.APPLICATION_JSON));
 
         expectFindStudentRequestToEsb(STUDENT_NUMBER, STUDENT_USERNAME);
+        expectFindStudentRequestToEsb(STUDENT_NUMBER_2, STUDENT_USERNAME_2);
         expectFindEmployeeRequestToEsb(TEACHER_ID_PREFIX + TEACHER_ID, TEACHER_USERNAME);
     }
+
 
 }
