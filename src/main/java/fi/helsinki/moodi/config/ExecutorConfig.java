539e792774744182390e5e509bea311a4512a57c
Marko Rautajoki <marko.rautajoki@gofore.com>
2017-02-16 15:42:08 +0200

    Update httpclient version. Increase Threadpool size

--- src/main/java/fi/helsinki/moodi/config/ExecutorConfig.java
+++ src/main/java/fi/helsinki/moodi/config/ExecutorConfig.java
@@ -29,8 +29,8 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class ExecutorConfig {
 
-    private static final int CORE_POOL_SIZE = 1;
-    private static final int MAX_POOL_SIZE = 1;
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAX_POOL_SIZE = 20;
     private static final int KEEP_ALIVE_SECONDS = 10;
 
     @Bean
