a7a58e87d54b1215af62a13f0304b3625b8f0546
Marko Rautajoki <markorautajoki@gmail.com>
2018-03-27 15:18:40 +0300

    Rename test cases. Add license

--- src/test/java/fi/helsinki/moodi/integration/oodi/OodiStudentApprovalStatusResolverTest.java
+++ src/test/java/fi/helsinki/moodi/integration/oodi/OodiStudentApprovalStatusResolverTest.java
@@ -1,5 +1,22 @@
-package fi.helsinki.moodi.integration.oodi;
+/*
+ * This file is part of Moodi application.
+ *
+ * Moodi application is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Moodi application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Moodi application.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+
+package fi.helsinki.moodi.integration.oodi;
 
 import fi.helsinki.moodi.test.AbstractMoodiIntegrationTest;
 import org.junit.Test;
@@ -14,23 +31,23 @@ public class OodiStudentApprovalStatusResolverTest extends AbstractMoodiIntegrat
     private OodiStudentApprovalStatusResolver oodiStudentApprovalStatusResolver;
 
     @Test
-    public void thatStudentIsNotApprovedIfNotAutomaticEnabledAndNotApproved() {
-        assertFalse(oodiStudentApprovalStatusResolver.isApproved(getOodiStudent(false, false)));
-    }
-
-    @Test
-    public void thatStudentIsApprovedIfNotAutomaticEnabledAndNotApproved() {
+    public void thatStatusIsResolvedToApprovedWhenApprovedAndAutomaticIsNotEnabled() {
         assertTrue(oodiStudentApprovalStatusResolver.isApproved(getOodiStudent(true, false)));
     }
 
     @Test
-    public void thatStudentIsNotApprovedIfAutomaticEnabledAndNonApprovedStatusCode() {
-        assertFalse(oodiStudentApprovalStatusResolver.isApproved(getOodiStudent(true, true, NON_APPROVED_ENROLLMENT_STATUS_CODE)));
+    public void thatStatusIsResolvedToNotApprovedWhenNotApprovedAndAutomaticIsNotEnabled() {
+        assertFalse(oodiStudentApprovalStatusResolver.isApproved(getOodiStudent(false, false)));
     }
 
     @Test
-    public void thatStudentIsApprovedIfAutomaticEnabledAndApprovedStatusCode() {
+    public void thatStatusIsResolvedToApprovedWhenAutomaticIsEnabledAndStatusCodeIsApproved() {
         assertTrue(oodiStudentApprovalStatusResolver.isApproved(getOodiStudent(true, true)));
+    }
+
+    @Test
+    public void thatStatusIsResolvedToNotApprovedWhenAutomaticIsEnabledAndStatusCodeIsNotApproved() {
+        assertFalse(oodiStudentApprovalStatusResolver.isApproved(getOodiStudent(true, true, NON_APPROVED_ENROLLMENT_STATUS_CODE)));
     }
 
     private OodiStudent getOodiStudent(boolean approved, boolean automaticEnabled) {
