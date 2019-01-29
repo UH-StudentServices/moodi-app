985e63132b1c5aa2b6484fab823a6d8746ee0034
Tero Hagström <tero.hagstrom@sugarmine.com>
2019-01-29 18:06:02 +0200

    MOODI-87 Oodista kurssilta poistettujen opiskelijoiden tulee poistua myös Moodlesta - Fixed a Checkstyle error.

--- src/main/java/fi/helsinki/moodi/service/synchronize/process/SynchronizingProcessor.java
+++ src/main/java/fi/helsinki/moodi/service/synchronize/process/SynchronizingProcessor.java
@@ -36,7 +36,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
