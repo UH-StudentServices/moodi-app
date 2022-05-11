529db3789e32812d40a9ce4cc4a954ce41368ff7
Hannu-Pekka Rajaniemi <hannu-pekka.rajaniemi@helsinki.fi>
2022-05-11 13:48:46 +0300

    MOODI-158 - test fixing characterset problems in integration

--- src/main/java/fi/helsinki/moodi/integration/moodle/MoodleClient.java
+++ src/main/java/fi/helsinki/moodi/integration/moodle/MoodleClient.java
@@ -33,6 +33,7 @@ import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -106,6 +107,7 @@ public class MoodleClient {
     public long createCourse(final MoodleCourse course) {
         final MultiValueMap<String, String> params = createParametersForFunction("core_course_create_courses");
 
+        params.set("_charset_", StandardCharsets.UTF_8.name());
         params.set(createParamName(COURSES, "idnumber", 0), course.idNumber);
         params.set(createParamName(COURSES, "fullname", 0), course.fullName);
         params.set(createParamName(COURSES, "shortname", 0), course.shortName);
