af94e10bf74cde56eaa1c115080742cb6160908e
Jouni Mäkeläinen <jouni@spanningtree.io>
2023-08-22 18:28:16 +0300

    MOODI-167: added support for local dockerized app development

--- src/main/java/fi/helsinki/moodi/config/RemoteServerDeploymentCondition.java
+++ src/main/java/fi/helsinki/moodi/config/RemoteServerDeploymentCondition.java
@@ -25,6 +25,6 @@ public final class RemoteServerDeploymentCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        return context.getEnvironment().acceptsProfiles("dev", "prod");
+        return context.getEnvironment().acceptsProfiles("dev", "prod", "local_docker");
     }
 }
